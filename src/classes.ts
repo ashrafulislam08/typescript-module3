af94f7dae0df8432007da1630444ddb086a7f631
Ashraful Islam <ashrafulislam.dev1@gmail.com>
2025-05-04 22:26:04 +0600

    basic class

--- src/classes.ts
+++ src/classes.ts
@@ -1,1 +1,24 @@
-// 
+// OOP - Class
+
+class Animal {
+    name: string;
+    species: string; 
+    sound: string;
+
+    constructor(name: string, species: string, sound: string) {
+        this.name = name;
+        this.species = species;
+        this.sound = sound;
+    }
+
+
+    makeSound() {
+        console.log(`The sound of ${this.name} is ${this.sound}`)
+    }
+}
+
+const cat = new Animal("Billa", 'Cat', "Mewo mewo")
+cat.makeSound()
+
+const dog = new Animal("German Shepard Bhai", "dog", "Ghew Ghew")
+dog.makeSound();
